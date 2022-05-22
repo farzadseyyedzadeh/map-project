9e18ba5552a0f5f3d07fbd727dc7d8b6713c500b
farzad seyyedzadeh <iamseyfarsey@gmail.com>
2022-05-22 19:46:59 +0430

    some changes

--- bootstrap/init.php
+++ bootstrap/init.php
@@ -1,11 +1,9 @@
 <?php
 session_start();
-
 include "constants.php";
 include BASE_PATH . "bootstrap/config.php";
 include BASE_PATH . "vendor/autoload.php";
 include BASE_PATH . "libs/helpers.php";
-
 try {
     $pdo = new PDO("mysql:dbname=$database_config->db;host={$database_config->host}", $database_config->user, $database_config->pass);
     $pdo->exec("set names utf8;");
