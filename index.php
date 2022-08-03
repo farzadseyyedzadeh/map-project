8b1cb8ecdae47d90127a82f3c74f32282bb223ad
farzad seyyedzadeh <85734789+farzadseyyedzadeh@users.noreply.github.com>
2022-08-03 22:10:16 +0430

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,8 @@
 include "bootstrap/init.php";
 
 $location = true;
+
 if(isset($_GET['loc']) and is_numeric($_GET['loc'])){
     $location = getLocation($_GET['loc']);
 }
 include "tpl/tpl-index.php";
-
-// this is test
